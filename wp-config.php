13816c127ecdda39a3f3b88c92daf8c39e146b95
Nangeso-Roy <jmnangeso@gmai.com>
2023-04-13 13:02:38 +0300

    Added the blocksy tenplate and used the barbarshop skin with Elementor

--- wp-config.php
+++ wp-config.php
@@ -19,9 +19,9 @@
  */
 
 // ** Database settings - You can get this info from your web host ** //
-/** The name of the database for WordPress */
-define( ‘FS_METHOD’, ‘direct’ );
+define( 'FS_METHOD', 'direct' );
 
+/** The name of the database for WordPress */
 define( 'DB_NAME', 'tmop' );
 
 /** Database username */
